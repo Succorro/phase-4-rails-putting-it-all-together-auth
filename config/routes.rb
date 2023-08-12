bfddf43400a149ec6ed7fde5fdd85b748ca18b57
Steven Gutierrez <stevogbmv@gmail.com>
2023-08-12 15:40:51 -0700

    commit tree 9271b218cbbbac07cf923b17641db6558c4ff1d3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  # User controller routes 
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  # Sessions Controller routes 
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  # Recipe controller routes 
+  get "/recipes", to: "recipes#index"
+  post "/recipes", to: "recipes#create"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
